02e6e84bae89635aad852544b4ffcf022efe416a
nam <ujma1234@naver.com>
2023-01-27 12:20:47 +0900

    Add ignore && Modify Linear output

--- KOBE.py
+++ KOBE.py
@@ -184,7 +184,7 @@ class LSBERT(nn.Module):
 
         self.month_classifier = nn.Linear(self.fc_size, 12)
         self.month_fc = nn.Linear(hidden_size, self.fc_size)
-        self.day_classifier = nn.Linear(self.fc_size, 30)
+        self.day_classifier = nn.Linear(self.fc_size, 31)
         self.day_fc = nn.Linear(hidden_size, self.fc_size)
         self.hour_classifier = nn.Linear(self.fc_size, 24)
         self.hour_fc = nn.Linear(hidden_size, self.fc_size)
@@ -229,55 +229,56 @@ class LSBERT(nn.Module):
 
 ##############################################################################
 
-num_epochs = 1000 
-learning_rate = 0.0001
+# num_epochs = 1000 
+# learning_rate = 0.0001
 
-model = LSBERT(hidden_size = 768, num_layers=64, fc_size = 2048)
+# model = LSBERT(hidden_size = 768, num_layers=64, fc_size = 2048)
 
-# Prepare optimizer and schedule (linear warmup and decay)
-no_decay = ['bias', 'LayerNorm.weight']
-optimizer_grouped_parameters = [
-    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-]
+# # Prepare optimizer and schedule (linear warmup and decay)
+# no_decay = ['bias', 'LayerNorm.weight']
+# optimizer_grouped_parameters = [
+#     {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+#     {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+# ]
 
-optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
-loss_fn = nn.CrossEntropyLoss()
+# optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+# loss_fn = nn.CrossEntropyLoss()
 
-t_total = len(train_dataloader) * num_epochs
-warmup_step = int(t_total * warmup_ratio)
+# t_total = len(train_dataloader) * num_epochs
+# warmup_step = int(t_total * warmup_ratio)
 
-scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
+# scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
 
-def calc_accuracy(X,Y):
-    max_vals, max_indices = torch.max(X, 1)
-    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
-    return train_acc
+# def calc_accuracy(X,Y):
+#     max_vals, max_indices = torch.max(X, 1)
+#     train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+#     return train_acc
 
-for e in range(num_epochs):
-    train_acc = 0.0
-    test_acc = 0.0
-    model.train()
-    for batch_id, (x, label) in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
-        optimizer.zero_grad()
-        label = label.long().to(device)
-        out = model(x)
-        loss = loss_fn(out, label)
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
-        optimizer.step()
-        scheduler.step()
-        train_acc += calc_accuracy(out, label)
-        if batch_id % log_interval == 0:
-            print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
-    print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
-    model.eval()
-    for batch_id, (x, label) in tqdm(enumerate(test_dataloader), total=len(test_dataloader)):
-        label = label.long().to(device)
-        out = model(x)
-        test_acc += calc_accuracy(out, label)
-    print("epoch {} test acc {}".format(e+1, test-acc) / (batch_id+1))
+# for e in range(num_epochs):
+#     train_acc = 0.0
+#     test_acc = 0.0
+#     model.train()
+#     for batch_id, (x, label) in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
+#         optimizer.zero_grad()
+#         label = label.long().to(device)
+#         out = model(x)
+#         loss = loss_fn(out, label)
+#         loss.backward()
+#         torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+#         optimizer.step()
+#         scheduler.step()
+#         train_acc += calc_accuracy(out, label)
+#         if batch_id % log_interval == 0:
+#             print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+#     print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
+#     model.eval()
+#     for batch_id, (x, label) in tqdm(enumerate(test_dataloader), total=len(test_dataloader)):
+#         label = label.long().to(device)
+#         out = model(x)
+#         test_acc += calc_accuracy(out, label)
+#     print("epoch {} test acc {}".format(e+1, test-acc) / (batch_id+1))
 
+###################################################################################################
 
     # pooler2 = torch.zeros(1,768, dtype = torch.float32)
     # pooler = []
